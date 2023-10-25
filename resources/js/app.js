8789f3c06d71be57aee63d93f142c0b38e2c74d7
fabri2605 <fabridp77@gmail.com>
2023-10-25 13:01:46 -0300

    comment_for_help

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,25 @@
 import './bootstrap';
 
+/*
+
+CODIGO QUE QUIERO EJECUTAR
+
+FUNCIONALIDAD: Cuando se ingresa un file en el input quiero que muestre el nombre,
+                para que el usuario sepa que esta cargado y no lo vuelva a hacer.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fileInput = document.getElementById('dropzone-file');
+    const uploadedFileName = document.getElementById('uploadedFileName');
+
+    fileInput.addEventListener('change', function () {
+        const files = fileInput.files;
+
+        if (files.length > 0) {
+            uploadedFileName.textContent = files[0].name;
+        } else {
+            uploadedFileName.textContent = '';
+        }
+    });
+});
+
+*/
